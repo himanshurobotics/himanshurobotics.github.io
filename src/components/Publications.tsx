fa07e258da64079903d92ffb30c1298c022dc8a8
Himanshu Varshney <varshney.himanshu111@gmail.com>
2025-11-23 17:03:04 +0530

    Update Publications.tsx

--- src/components/Publications.tsx
+++ src/components/Publications.tsx
@@ -4,14 +4,26 @@ import { Calendar } from 'lucide-react';
 const Publications = () => {
   const publicationsData = [
     {
+      title: "Forward Kinematics Solution of a 6-DOF All-Revolute Parallel Manipulator Using Neural Network",
+      publication: "International Journal of Computational Methods (World Scientific)",
+      date: "September 26th, 2025",
+      description: "Neural Network-based Forward Kinematics solver for 6-DOF All-Revolute Parallel Manipulators to ensure high-accuracy positioning in real-time applications. The approach uniquely integrates the model with a digital twin and physical hardware, facilitating seamless virtual mapping and compliant manipulation."
+    },
+    {
+      title: "Adaptive Impedance Control for Synchronized and Compliant Manipulation in the Dual 7-DoF Arms Robot",
+      publication: "Advances in Robotics (AIR) 2025 (ACM)",
+      date: "July 2nd-5th, 2025",
+      description: "Impedance control framework for dual 7-DoF humanoid arms that achieves synchronized motion and compliant adaptability against unknown environmental forces. Simulation results validate the framework's ability to ensure robust trajectory tracking and stable interaction within dynamic environments."
+    },
+    {
       title: "Framework for implementing and investigating sensorless force control for collaborative manipulation using parallel robots",
       publication: "Multibody System Dynamics (Springer)",
       date: "June 30th, 2025",
-      description: "This research focuses on implementing and evaluating indirect force control strategies (impedance and admittance control) for all revolute parallel manipulators to ensure safe and efficient human-robot interaction by mitigating high interaction forces while maintaining their inherent benefits like high accuracy, stiffness and backdrivability."
+      description: "Implementing and evaluating indirect force control strategies (impedance and admittance control) for all revolute parallel manipulators to ensure safe and efficient human-robot interaction by mitigating high interaction forces while maintaining their inherent benefits like high accuracy, stiffness and backdrivability."
     },
     {
       title: "Non-linear fractional order fuzzy PD plus I controller for trajectory optimization of 6-DOF modified Puma 560 robotic arm",
-      publication: "Proceedings of Congress on Control, Robotics (CRM 2023), and Mechatronics, Smart Innovation, Systems and Technologies (Springer)",
+      publication: "Proceedings of Congress on Control, Robotics, and Mechatronics (CRM) 2023. Book: Smart Innovation, Systems and Technologies (Springer)",
       date: "November 11th, 2023",
       description: "Employed the non-integer order calculus to enhance the control action of the non-linear fractional order fuzzy PD plus I (FOFPD+I) controller to control a serial robotic arm."
     }
